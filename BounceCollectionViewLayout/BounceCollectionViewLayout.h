b81c991f57ece4c173fb3517becc2891899a7228
viewDidAppear <ben.deckys@paypay-corp.co.jp>
2021-10-18 23:03:36 +0900

    Initial Commit

---
+++ BounceCollectionViewLayout/BounceCollectionViewLayout.h
@@ -0,0 +1,18 @@
+//
+//  BounceCollectionViewLayout.h
+//  BounceCollectionViewLayout
+//
+//  Created by Ben Deckys on 2021/10/18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BounceCollectionViewLayout.
+FOUNDATION_EXPORT double BounceCollectionViewLayoutVersionNumber;
+
+//! Project version string for BounceCollectionViewLayout.
+FOUNDATION_EXPORT const unsigned char BounceCollectionViewLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BounceCollectionViewLayout/PublicHeader.h>
+
+
